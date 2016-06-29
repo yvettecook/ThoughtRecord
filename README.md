9c8a524308e84d90bdc85751772c851b970a6e1d
Yvette <yvettecook@gmail.com>
2016-06-29 22:22:24 +0100

    Basic steps for thought record is there

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 An app to record your thoughts in the moment, and challenge them. To assist with the CBT process.
 
 > "The thought record is one of the fundamental tools in cognitive behavioral therapy (CBT). The underlying principle can be summarised as "what do you believe, and why do you believe it?"."
+
+
+### Spec
+
+* A list of thought records
+* Add a new thought record
+* Answer the 7 questions
+* View previous thought records
--- ThoughtRecord/ViewController.swift
+++ ThoughtRecord/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import ResearchKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ORKTaskViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let step1 = ORKInstructionStep(identifier: "intro")
+        step1.title = "Welcome to ResearchKit"
+
+        let format = ORKTextAnswerFormat()
+
+        let question1 = ORKQuestionStep(identifier: "question1", title: "Situation / Trigger", answer: format)
+        question1.text = "Body sensations"
+
+        let question2 = ORKQuestionStep(identifier: "question2", title: "Feelings / Emotions", answer: format)
+        question2.text = "What emotion did I feel at that time? What else? How intense was it? What did I notice in my body? Where did I feel it?  SCALE 1-100"
+
+        let question3 = ORKQuestionStep(identifier: "question3", title: "Unhelpful Thoughts / Images", answer: format)
+        question3.text = "What went through my mind? What disturbed me? What did those thoughts/images/memories mean to me, or say about me or the situation? What am I responding to? What ‘button’ is this pressing for me? What would be the worst thing about that, or that could happen?"
+
+        let question4 = ORKQuestionStep(identifier: "question4", title: "Facts that support the unhelpful thought", answer: format)
+        question4.text = "What are the facts? What facts do I have that the unhelpful thought/s are totally true?"
+
+        let question5 = ORKQuestionStep(identifier: "question5", title: "Facts that provide evidence against the unhelpful thought", answer: format)
+        question5.text = "What facts do I have that the unhelpful thought/s are NOT totally true? Is it possible that this is opinion, rather than fact? What have others said about this?"
+
+        let question6 = ORKQuestionStep(identifier: "question6", title: "Alternative, more realistic and balanced perspective", answer: format)
+        question6.text = "STOPP! Take a breath.... What would someone else say about this situation? What’s the bigger picture? Is there another way of seeing it? What advice would I give someone else? Is my reaction in proportion to the actual event? Is this really as important as it seems?"
+
+        let question7 = ORKQuestionStep(identifier: "question7", title: "Outcome / Re-rate emotion", answer: format)
+        question7.text = "What am I feeling now? (0-100%) SCALE 1-100 What could I do differently? What would be more effective? Do what works! Act wisely. What will be most helpful for me or the situation? What will the consequences be?"
+
+        let task1 = ORKOrderedTask(identifier: "task", steps: [step1, question1, question2, question3, question4, question5, question6, question7])
+
+        let taskViewController = ORKTaskViewController(task: task1, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
+
+    func taskViewController(taskViewController: ORKTaskViewController,
+                            didFinishWithReason reason: ORKTaskViewControllerFinishReason,
+                                                error: NSError?) {
+        let taskResult = taskViewController.result
+        // You could do something with the result here.
+        print("task result: \(taskResult)")
+
+        // Then, dismiss the task view controller.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
